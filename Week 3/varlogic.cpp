7711bf75431559e93f4250451a98f4381ec5fea8
IanCookThatCertainFella <thatbeardedguy4@gmail.com>
2025-02-04 12:18:41 -0600

    Week 3 Part 2
    Learned variables and some logic to go with them.

--- Week 3/varlogic.cpp
+++ Week 3/varlogic.cpp
@@ -29,4 +29,34 @@ int main() {
 	if (false) {
 		cout << "This line of code will never run \n";
 	} //end of if(false)
+
+	//variables!
+	//integets, floats, doubles, characters, strings and booleans
+	//we're making a new variable of the type "int" awith the name "playerScore",
+	//with the value of 255
+	int playerScore = 255;
+
+	//declaring (and defining) the variable, type int, name = playerTwoScore;
+	int playerTwoScore = 0;
+
+	//constant values cannot change their value, generally written in all caps so they can be seen
+	//in a crowd 
+	const float EARTH_GRAVITY = -9.81f;	//acceleration in meters/second
+
+	cout << "Player 1 score = " << playerScore << ".\n";
+	cout << "Player 2 score = " << playerTwoScore << ".\n";
+	cout << "Earth's gravity is " << EARTH_GRAVITY << " meters/second.\n";
+
+	//we're making a constant variable, type double, named PI, with the value of 3.14159
+	const double PI = 3.14159;
+
+	char answer = 'n';	//this stores the user's input
+
+	cout << "Would you like to keep playing?\n";	//ask the user a qustion
+	cin >> answer;	                                //get input
+
+	if (answer == 'y') {	                        //test the input
+		cout << "Hooray! Let's do it again!\n";	    //print text
+		return 0;	                                //quit the program early
+	}
 } //this is the end of main
